fd5cb565b36c3483e7cf232a1cc3b9d5ef1108bd
NSTechBytes <nstechbytes@gmail.com>
2024-11-14 10:04:28 +0500

    Add files via upload

---
+++ SourceCode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Drawing;
+using Rainmeter;
+
+namespace MousePositionPlugin
+{
+    public class Measure
+    {
+        private string type;
+
+        internal void Reload(API api, ref double maxValue)
+        {
+            type = api.ReadString("MeasureType", "MouseX").ToLowerInvariant();
+        }
+
+        internal double Update()
+        {
+            if (type == "mousex")
+            {
+                return Cursor.Position.X;
+            }
+            else if (type == "mousey")
+            {
+                return Cursor.Position.Y;
+            }
+
+            return 0.0;
+        }
+    }
+
+    public static class Plugin
+    {
+        [DllExport]
+        public static void Initialize(ref IntPtr data, IntPtr rm)
+        {
+            data = GCHandle.ToIntPtr(GCHandle.Alloc(new Measure()));
+        }
+
+        [DllExport]
+        public static void Finalize(IntPtr data)
+        {
+            GCHandle.FromIntPtr(data).Free();
+        }
+
+        [DllExport]
+        public static void Reload(IntPtr data, IntPtr rm, ref double maxValue)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            measure.Reload(new API(rm), ref maxValue);
+        }
+
+        [DllExport]
+        public static double Update(IntPtr data)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            return measure.Update();
+        }
+    }
+}
